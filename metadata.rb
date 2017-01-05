100ffdeb91436d1eef94f038dfd19381379f1a33
amarkunamneni999 <amarkunamneni@gmail.com>
2017-01-05 10:41:55 -0500

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'tomcat'
+maintainer       'Amar'
+maintainer_email 'amar'
+license          'All rights reserved'
+description      'Installs/Configures tomcat'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
